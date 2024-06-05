e19c51c2ea11f52a721ac0b15b368c240693571c
Josh Hackney <jhaxllc@gmail.com>
2024-06-04 21:23:15 -0400

    added a config file for static values so I don't have to pass them around w. props anymore

---
+++ src/Services/Config.jsx
@@ -0,0 +1,13 @@
+export const Config = {
+    // apiKey: "<YOUR_GoogleAPIKey_HERE>",
+    apiURL: "http://jhaxdev.com:3000/api",
+    endpoints: {
+      auth: "/auth/",
+      login: "/users/login/",
+      chats: "/chats/",
+      messages: {
+        all: '/messages/',
+        new: '/messages/new'
+      }
+    }
+  };
